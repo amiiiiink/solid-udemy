6ca63549700170dc622399919b5d439b6bc7b4d3
a.karimi <a.karimi@cloud.ir>
2024-10-21 12:09:37 +0330

    develop route and area calculator class

---
+++ app/Patterns/AreaCalculator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Patterns;
+
+class AreaCalculator
+{
+    public function calculate()
+    {
+        return 'hello world ... ';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,12 @@ Route::get('/', function () {
 });
 
 Route::post('order/{product_id}/process', ProcessOrdersController::class);
+Route::get('area', function(\App\Patterns\AreaCalculator $areaCalculator) {
+
+//    $square = new \App\Patterns\Square(10,10);
+//    $triangle = new \App\Patterns\Triangle(10,6);
+//    $circle = new \App\Patterns\Circle(10);
+
+    return $areaCalculator->calculate();
+
+});
