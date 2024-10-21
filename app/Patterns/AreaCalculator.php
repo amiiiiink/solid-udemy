9139b455418bb26be6c6c5dcdcddc222cc63d71a
a.karimi <a.karimi@cloud.ir>
2024-10-21 12:29:57 +0330

    develop sqaure class to calculate area of square

--- app/Patterns/AreaCalculator.php
+++ app/Patterns/AreaCalculator.php
@@ -2,10 +2,25 @@
 
 namespace App\Patterns;
 
+//square class
+class Square
+{
+    public mixed $height;
+    public mixed $width;
+
+    public function __construct($height, $width)
+    {
+        $this->height = $height;
+        $this->width = $width;
+    }
+}
+
+
+
 class AreaCalculator
 {
-    public function calculate()
+    public function calculate($shape): float|int
     {
-        return 'hello world ... ';
+        return $shape->width * $shape->height;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,18 @@
 <?php
 use App\Http\Controllers\ProcessOrdersController;
+use App\Patterns\AreaCalculator;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::post('order/{product_id}/process', ProcessOrdersController::class);
-Route::get('area', function(\App\Patterns\AreaCalculator $areaCalculator) {
+Route::get('area', function(AreaCalculator $areaCalculator) {
 
 //    $square = new \App\Patterns\Square(10,10);
 //    $triangle = new \App\Patterns\Triangle(10,6);
 //    $circle = new \App\Patterns\Circle(10);
-
-    return $areaCalculator->calculate();
+    $sqaure = new \App\Patterns\Square(10,20);
+    return $areaCalculator->calculate($sqaure);
 
 });
