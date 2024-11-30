cbeddb08bb0f276155eb4c5b0c1ea5465432340f
a.karimi <a.karimi@cloud.ir>
2024-11-30 13:45:42 +0330

    implement another basic example of CarInterface

---
+++ app/InterfaceSegregation/base3.php
@@ -0,0 +1,46 @@
+<?php
+
+interface CarInterface
+{
+    public function shiftGear();
+    public function getFuel();
+    public function steer();
+}
+
+class ElectricCar implements CarInterface
+{
+
+    public function shiftGear()
+    {
+        // TODO: Implement shiftGear() method.
+    }
+
+    public function getFuel()
+    {
+        // TODO: Implement getFuel() method.
+    }
+
+    public function steer()
+    {
+        // TODO: Implement steer() method.
+    }
+}
+
+class AutomaticCar implements CarInterface
+{
+
+    public function shiftGear()
+    {
+        // TODO: Implement shiftGear() method.
+    }
+
+    public function getFuel()
+    {
+        // TODO: Implement getFuel() method.
+    }
+
+    public function steer()
+    {
+        // TODO: Implement steer() method.
+    }
+}
